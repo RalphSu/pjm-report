f9091580ef47c751e78085340fbeadb269122387
ralph <keyi@localhost.(none)>
2014-05-12 10:54:21 +0000

    fix daemon

--- report/host_daemon.sh
+++ report/host_daemon.sh
@@ -2,19 +2,20 @@
 
 
 port='8080'
-`curl http://127.0.0.1:$port > /dev/null`
+`curl --max-time 3 http://127.0.0.1:$port > /dev/null`
 ret=`echo $?`
-echo $ret
+echo "$(date) curl response $ret"
 if [ $ret -eq "0" ]; then
-	echo 'server is ok exit'
+	echo "$(date) server is ok exit"
 	exit
 fi
 
 pid=`ps aux | grep ruby | grep -v grep | awk '{print $2}'`
-echo $pid
+echo " old server pid is $pid"
 
-if [ -z "$VAR" ]; then 
-	echo "restart server"
+if [ ! -z "$pid" ]; then 
+	echo "$(date) kill old server"
 	`kill -9 $pid`
 fi
-`cd ~/workspace/pjm2/; ruby script/server -e production -p 8080 -d;`
+echo "$(date) start new server"
+`cd ~/workspace/pjm2/; ruby script/server -e production -p 8080 -d > /dev/null;`
